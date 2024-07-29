63a13ff5390c9a10d0965d04975e036633f160e1
Breno Sobral <ssobralbreno@gmail.com>
2024-07-29 15:58:49 -0300

    feat: finish firebot crons

--- src/core/constant/constant.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-export const TIBIA_DATA_API = process.env.TIBIA_DATA_API;
--- src/external/get-guilds.tibia-api.external.ts
+++ src/external/get-guilds.tibia-api.external.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import { TIBIA_DATA_API } from '../core/constant/constant';
 import { ApiResponse } from '../core/interfaces/guilds.interface';
 
 export class GetGuildsTibiaData {
   async getGuilds(): Promise<ApiResponse> {
-    const url = `${TIBIA_DATA_API}/v4/guild/Unebro`;
-    console.log(url);
+    const url = `https://api.tibiadata.com//v4/guild/Unebro`;
     try {
       const headers = {
         'Content-Type': 'application/json',
